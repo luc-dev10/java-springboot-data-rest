d54424461159d340ce7968c4596e87007160bc0a
luc-dev10 <luc.dev1008@gmail.com>
2022-02-22 21:33:47 -0500

    add custom path for jpa repo

--- src/main/java/com/lucio/springboot/demo/dao/EmployeeJpaRepository.java
+++ src/main/java/com/lucio/springboot/demo/dao/EmployeeJpaRepository.java
@@ -2,6 +2,8 @@ package com.lucio.springboot.demo.dao;
 
 import com.lucio.springboot.demo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "/members")
 public interface EmployeeJpaRepository extends JpaRepository<Employee, Integer> {
 }
